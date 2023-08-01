1612fa0cdbadbe03fb08d5585eae73bb9144a0f0
nefelibatala1993 <leuela1993@gmail.com>
2023-08-01 11:06:12 +0300

    add extra functionality to the lodash function

--- 01-utilizing-packages-effectively/index.js
+++ 01-utilizing-packages-effectively/index.js
@@ -4,13 +4,17 @@ const _ = require('lodash');
 const array = [1, 2, 3, [1, 2, 3, [1, 2, 3, [1, 2, 3]]]];
 const flattendArray = _.flattenDeep(array);
 
-console.log(`
-  The flattened array "${flattendArray}", and checking if it is an array: ${Array.isArray(flattendArray).toString()}
-`);
+console.log(`The flattened array "${flattendArray}", and checking if it is an array: ${Array.isArray(flattendArray).toString()}\n`);
 
 // Sorting an unsorted array using Lodash
 const randomArray = [87, 12, 45, 64, 3, 98, 21, 77, 33, 55];
 
-console.log(`${_.sortBy(randomArray)}`);
-console.log(`${_.max(randomArray)}`);
-console.log(`${_.filter(randomArray, (num) => num > 50)}`);
+console.log(`Sorting the array: ${_.sortBy(randomArray)}`);
+console.log(`Maximum Value is: ${_.max(randomArray)}`);
+console.log(`Filter the array: ${_.filter(randomArray, (num) => num > 50)}`, '\n');
+
+// How to use the chuck function of lodash
+const originalArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const chunkedArray = _.chunk(originalArray, 2);
+
+console.log(chunkedArray, '\n');
