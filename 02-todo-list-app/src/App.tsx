8c1ecedfb71090d707db03e7f7cf5bf26f65cd65
nefelibatala1993 <leuela1993@gmail.com>
2023-08-02 15:18:35 +0300

    made some changes

--- 02-todo-list-app/src/App.tsx
+++ 02-todo-list-app/src/App.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import './App.css';
+import InputField from "./components/InputField";
+import { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">Hello World</div>
-  );
+const App: React.FC = () => {
+    const [ todo, setTodo  ] = useState<string>('');
+    const [ todos, setTodos ] = useState();
+
+    return (
+      <div className="App">
+        <span className="heading">Taskify</span>
+          <InputField todo={todo} setTodo={setTodo}/>
+      </div>
+    );
 }
 
 export default App;
